9c2c624b1585aceb573765dea7aea800766c758e
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-08-06 17:14:58 +0200

    Added basics of a FileTree

---
+++ files-main/src/test/java/de/team33/files/ui/FileTreeTrial.java
@@ -0,0 +1,38 @@
+package de.team33.files.ui;
+
+import de.team33.patterns.serving.alpha.Component;
+import de.team33.patterns.serving.alpha.Variable;
+import de.team33.sphinx.alpha.visual.JSplitPanes;
+
+import javax.swing.*;
+import java.nio.file.Path;
+import java.util.function.UnaryOperator;
+
+final class FileTreeTrial extends SwingTrial {
+
+    private static final UnaryOperator<Path> NORMAL_PATH = path -> path.toAbsolutePath().normalize();
+
+    private FileTreeTrial() {
+        this(new Component<>(NORMAL_PATH, Path.of(".")));
+    }
+
+    private FileTreeTrial(final Variable<Path> path) {
+        super(trialPane(path));
+        path.retrieve(this::setPath);
+    }
+
+    private static JSplitPane trialPane(final Variable<Path> path) {
+        return JSplitPanes.builder()
+                          .setLeftComponent(new JScrollPane(FileTree.using(path)))
+                          .setRightComponent(new JScrollPane(FileTree.using(path)))
+                          .build();
+    }
+
+    public static void main(final String[] args) {
+        SwingUtilities.invokeLater(new FileTreeTrial());
+    }
+
+    private void setPath(final Path path) {
+        setTitle(path.toAbsolutePath().normalize().toString());
+    }
+}
