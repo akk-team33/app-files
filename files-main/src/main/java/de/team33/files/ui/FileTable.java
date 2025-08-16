4f0803dc1320fd98366eed6725f72f9b972c662d
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-08-16 14:16:38 +0200

    slightly refactored FileTable

--- files-main/src/main/java/de/team33/files/ui/FileTable.java
+++ files-main/src/main/java/de/team33/files/ui/FileTable.java
@@ -8,15 +8,24 @@ import net.team33.fscalc.ui.rsrc.Ico;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.nio.file.Path;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.List;
+import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class FileTable {
 
+    private final TableCellRenderer fileEntryRenderer = new JTable().getDefaultRenderer(String.class);
+    private final TableCellRenderer lastModifiedRenderer = new JTable().getDefaultRenderer(String.class);
     private final JTable table;
     private final Component component;
 
@@ -28,8 +37,8 @@ public class FileTable {
 //                                                          .on(de.team33.sphinx.alpha.activity.Event.MOUSE_CLICKED,
 //                                                              new InfoTable.HEADER_MOUSE_LISTENER(jTable, context)::mouseClicked))
 //                            .setRowHeight(cr.getPreferredSize().height + 3)
-//                            .setDefaultRenderer(FileInfo.class, cr)
-                            .setDefaultRenderer(FileEntry.class, this::newCell)
+                            .setDefaultRenderer(FileEntry.class, this::newFileEntryCell)
+                            .setDefaultRenderer(Instant.class, this::newLastModifiedCell)
                             .setShowGrid(false)
                             .setRowSelectionAllowed(true)
                             .setColumnSelectionAllowed(false)
@@ -41,50 +50,74 @@ public class FileTable {
         this.component = new JScrollPane(table);
     }
 
-    private Component newCell(final JTable table,
-                              final Object value,
-                              final boolean isSelected,
-                              final boolean hasFocus,
-                              final int row,
-                              final int col) {
-        final FileEntry entry = (FileEntry) value;
-        final Column column = Column.values()[col];
-        return JLabels.builder()
-                      .setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 3))
-                      .setForeground(isSelected ? table.getSelectionForeground() : table.getForeground())
-                      .setBackground(isSelected ? table.getSelectionBackground() : table.getBackground())
-                      .setOpaque(isSelected)
-                      .setText(column.toText.apply(entry))
-                      .setIcon(column.toIcon.apply(entry))
-                      //.setFileInfo((FileInfo) value, table.convertColumnIndexToModel(column))
-                      .build();
-    }
-
     public static FileTable serving(final Retrievable<? extends Path> path) {
         return new FileTable(path);
     }
 
+    private static <R> R cast(final Class<R> rClass, final Object value) {
+        if (rClass.isInstance(value)) {
+            return rClass.cast(value);
+        } else {
+            final Class<?> vClass = (null == value) ? null : value.getClass();
+            throw new IllegalStateException(
+                    "<value> is expected to be an instance of %s - but was %s".formatted(rClass, vClass));
+        }
+    }
+
+    private static JLabel jLabel(final Object candidate) {
+        return cast(JLabel.class, candidate);
+    }
+
+    private Component newLastModifiedCell(final JTable jTable,
+                                          final Object value,
+                                          final boolean isSelected,
+                                          final boolean hasFocus,
+                                          final int row,
+                                          final int col) {
+        final Component result =
+                lastModifiedRenderer.getTableCellRendererComponent(jTable, value, isSelected, hasFocus, row, col);
+        jLabel(result).setText(LocalDateTime.ofInstant(cast(Instant.class, value), ZoneId.systemDefault())
+                                            .format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+                                                                     .withLocale(Locale.getDefault())));
+        return result;
+    }
+
+    @SuppressWarnings("MethodWithTooManyParameters")
+    private Component newFileEntryCell(final JTable jTable,
+                                       final Object value,
+                                       final boolean isSelected,
+                                       final boolean hasFocus,
+                                       final int row,
+                                       final int col) {
+        final Component result =
+                fileEntryRenderer.getTableCellRendererComponent(jTable, value, isSelected, hasFocus, row, col);
+        final FileEntry entry = cast(FileEntry.class, value);
+        return JLabels.charger(jLabel(result))
+                      .setText(entry.name())
+                      .setIcon(entry.isDirectory() ? Ico.CLSDIR : Ico.FILE)
+                      .charged();
+    }
+
     public final Component component() {
+        DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.FULL);
         return component;
     }
 
     private enum Column {
-        NAME("Name", FileEntry::name, entry -> entry.isDirectory() ? Ico.CLSDIR : Ico.FILE),
-        @SuppressWarnings("ReturnOfNull")
-        UPDATE("Last Modified", entry -> entry.lastModified().toString(), entry -> null),
-        @SuppressWarnings("ReturnOfNull")
-        SIZE("Size", entry -> String.valueOf(entry.size()), entry -> null);
+        NAME("Name", FileEntry.class, Function.identity()),
+        UPDATE("Last Modified", Instant.class, FileEntry::lastModified),
+        SIZE("Size", Long.class, FileEntry::size);
 
         private final String title;
-        private final Function<FileEntry, String> toText;
-        private final Function<FileEntry, Icon> toIcon;
+        private final Class<?> type;
+        private final Function<FileEntry, ?> toValue;
 
         <T> Column(final String title,
-                   final Function<FileEntry, String> toText,
-                   final Function<FileEntry, Icon> toIcon) {
+                   final Class<T> type,
+                   final Function<FileEntry, T> toValue) {
             this.title = title;
-            this.toText = toText;
-            this.toIcon = toIcon;
+            this.type = type;
+            this.toValue = toValue;
         }
 
         private static Column of(final String title) {
@@ -101,6 +134,10 @@ public class FileTable {
 
         private Model(final Retrievable<? extends Path> path) {
             path.retrieve(this::onSetPath);
+        }
+
+        private static Column column(final int index) {
+            return Column.values()[index];
         }
 
         private void onSetPath(final Path path) {
@@ -122,23 +159,24 @@ public class FileTable {
         }
 
         @Override
-        public final Object getValueAt(final int rowIndex, final int columnIndex) {
-            return entries.get(rowIndex);
+        public final Object getValueAt(final int rowIndex, final int colIndex) {
+            return column(colIndex).toValue.apply(entries.get(rowIndex));
         }
 
         @Override
-        public final String getColumnName(final int column) {
-            return Column.values()[column].title;
+        public final String getColumnName(final int colIndex) {
+            return column(colIndex).title;
         }
 
         @Override
         public final int findColumn(final String columnName) {
-            return List.of(Column.values()).indexOf(Column.of(columnName));
+            return List.of(Column.values())
+                       .indexOf(Column.of(columnName));
         }
 
         @Override
-        public final Class<?> getColumnClass(final int column) {
-            return FileEntry.class;
+        public final Class<?> getColumnClass(final int colIndex) {
+            return column(colIndex).type;
         }
     }
 }
