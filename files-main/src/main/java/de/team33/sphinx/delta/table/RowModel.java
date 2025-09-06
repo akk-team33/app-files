489fd7075fbd040942f5377341b8456eb7af5122
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-06 15:18:45 +0200

    Start working on a new RowModel

---
+++ files-main/src/main/java/de/team33/sphinx/delta/table/RowModel.java
@@ -0,0 +1,91 @@
+package de.team33.sphinx.delta.table;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.util.List;
+
+/**
+ * A basic {@link TableModel} for {@linkplain JTable tables} that essentially display a {@link List} of elements
+ * of a specific type, with each row of the table representing one of these elements.
+ * The table's columns are then determined by the individual properties of the elements.
+ *
+ * @param <E> The type of list elements, each of which is represented as a table row.
+ */
+public abstract class RowModel<E> extends AbstractTableModel {
+
+    /**
+     * Returns the currently valid {@link List} of elements that should be displayed as one table row each.
+     */
+    protected abstract List<? extends E> rows();
+
+    /**
+     * Returns a {@link List} of {@link Column}s describing the columns of the table in question.
+     */
+    protected abstract List<? extends Column<E>> columns();
+
+    @Override
+    public final int getRowCount() {
+        return rows().size();
+    }
+
+    @Override
+    public final int getColumnCount() {
+        return columns().size();
+    }
+
+    @Override
+    public final String getColumnName(final int columnIndex) {
+        return columns().get(columnIndex).title();
+    }
+
+    @Override
+    public final Class<?> getColumnClass(final int columnIndex) {
+        return columns().get(columnIndex).type();
+    }
+
+    @Override
+    public final boolean isCellEditable(final int rowIndex, final int columnIndex) {
+        // TODO? preliminary ...
+        return super.isCellEditable(rowIndex, columnIndex);
+    }
+
+    @Override
+    public final Object getValueAt(final int rowIndex, final int columnIndex) {
+        final E row = rows().get(rowIndex);
+        return columns().get(columnIndex).map(row);
+    }
+
+    @Override
+    public final void setValueAt(final Object aValue, final int rowIndex, final int columnIndex) {
+        // TODO? preliminary ...
+        super.setValueAt(aValue, rowIndex, columnIndex);
+    }
+
+    /**
+     * Represents a table column description to be used in the context of a {@link RowModel}.
+     *
+     * @param <E> The type of list elements, each of which is represented as a table row.
+     */
+    public interface Column<E> {
+
+        /**
+         * Returns the title of <em>this</em> column.
+         */
+        String title();
+
+        /**
+         * Returns the {@link Class} that represents the type of property to be displayed in <em>this</em> column.
+         * <p>
+         * A result of {@link #map(Object)} must match that type.
+         */
+        Class<?> type();
+
+        /**
+         * Returns the property value belonging to <em>this</em> column from an element.
+         * <p>
+         * A result must match {@link #type()}.
+         */
+        Object map(E element);
+    }
+}
---
+++ files-main/src/test/java/de/team33/sphinx/delta/table/RowModelTrial.java
@@ -0,0 +1,90 @@
+package de.team33.sphinx.delta.table;
+
+import de.team33.files.testing.SwingTrial;
+import de.team33.files.ui.Context;
+import de.team33.files.ui.FileTree;
+import de.team33.patterns.serving.alpha.Retrievable;
+import de.team33.sphinx.metis.JSplitPanes;
+import de.team33.sphinx.metis.JTables;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.List;
+import java.util.function.Function;
+
+import static de.team33.patterns.serving.alpha.Retrievable.Mode.INIT;
+import static javax.swing.JTable.AUTO_RESIZE_OFF;
+import static javax.swing.SwingConstants.*;
+
+final class RowModelTrial extends SwingTrial {
+
+    @SuppressWarnings("StaticCollection")
+    private static final List<FileColumn<?>> COLUMNS = List.of(
+            new FileColumn<>("Name", String.class, LEFT, File::getName),
+            new FileColumn<>("Last Modified", Instant.class, CENTER, file -> Instant.ofEpochMilli(file.lastModified())),
+            new FileColumn<>("Size", Long.class, RIGHT, File::length));
+
+    private final FileTree.Context context = new Context();
+    private final TableModel model = new FileModel(context.cwd());
+    private final JTable fileTable = JTables.builder(model)
+                                            .setAutoCreateRowSorter(true)
+                                            .setAutoResizeMode(AUTO_RESIZE_OFF)
+                                            .build();
+
+    public static void main(final String[] args) {
+        run(new RowModelTrial());
+    }
+
+    @Override
+    protected Container contentPane() {
+        return JSplitPanes.builder()
+                          //.setOrientation(JSplitPane.VERTICAL_SPLIT)
+                          .setLeftComponent(FileTree.by(context).component())
+                          .setRightComponent(new JScrollPane(fileTable))
+                          .build();
+    }
+
+    @Override
+    protected void setupFrame(final JFrame jFrame) {
+        context.cwd().subscribe(INIT, path -> jFrame.setTitle(path.toString()));
+    }
+
+    private record FileColumn<C>(String title, Class<C> type, int horizontalAlignment, Function<File, C> mapping)
+            implements RowModel.Column<File> {
+
+        @Override
+        public C map(final File element) {
+            return mapping.apply(element);
+        }
+    }
+
+    private static class FileModel extends RowModel<File> {
+
+        private volatile List<File> files = List.of();
+
+        FileModel(final Retrievable<? extends Path> cwd) {
+            cwd.subscribe(INIT, this::onSetCWD);
+        }
+
+        private void onSetCWD(final Path path) {
+            this.files = List.of(path.toFile().listFiles());
+            fireTableDataChanged();
+        }
+
+        @Override
+        protected final List<? extends File> rows() {
+            // Already is an immutable List ...
+            // noinspection AssignmentOrReturnOfFieldWithMutableType
+            return files;
+        }
+
+        @Override
+        protected final List<FileColumn<?>> columns() {
+            return COLUMNS;
+        }
+    }
+}
