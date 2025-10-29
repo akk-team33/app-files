fc0b260499cc0f7c2fc76feb0a825eda4305b69b
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-10-29 13:36:33 +0100

    Ensure cwd to be a directory path

--- files-main/src/main/java/de/team33/files/phobos/Files.java
+++ files-main/src/main/java/de/team33/files/phobos/Files.java
@@ -2,6 +2,7 @@ package de.team33.files.phobos;
 
 import de.team33.files.phobos.ui.Frame;
 import de.team33.patterns.execution.metis.SimpleAsyncExecutor;
+import de.team33.patterns.io.delta.FileEntry;
 import de.team33.patterns.serving.alpha.Component;
 import de.team33.patterns.serving.alpha.Variable;
 import de.team33.sphinx.lambda.SwingApp;
@@ -9,11 +10,9 @@ import de.team33.sphinx.lambda.SwingApp;
 import javax.swing.*;
 import java.nio.file.Path;
 import java.util.concurrent.Executor;
-import java.util.function.UnaryOperator;
 
 public class Files extends SwingApp {
 
-    private static final UnaryOperator<Path> NORMAL_PATH = path -> path.toAbsolutePath().normalize();
     private final Executor executor = new SimpleAsyncExecutor();
 
     public static void main(final String[] args) {
@@ -28,7 +27,12 @@ public class Files extends SwingApp {
     @SuppressWarnings("ClassNameSameAsAncestorName")
     private class Context implements Frame.Context {
 
-        private final Variable<Path> cwd = new Component<>(executor, NORMAL_PATH, Path.of("."));
+        private Path validPath(final Path path) {
+            final FileEntry entry = FileEntry.of(path);
+            return entry.isDirectory() ? entry.path() : cwd.get();
+        }        private final Variable<Path> cwd = new Component<>(executor, this::validPath, Path.of("."));
+
+
 
         @Override
         public final Variable<Path> cwd() {
