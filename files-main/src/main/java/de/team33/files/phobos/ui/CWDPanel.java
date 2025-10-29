1f001fd5ff860885073cee210c4fbb77a9f47725
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-10-29 13:51:59 +0100

    Refactored CWDPanel to associate Folder Symbol with Text Field

--- files-main/src/main/java/de/team33/files/phobos/ui/CWDPanel.java
+++ files-main/src/main/java/de/team33/files/phobos/ui/CWDPanel.java
@@ -18,46 +18,21 @@ import static de.team33.patterns.serving.alpha.Retrievable.Mode.INIT;
 final class CWDPanel {
 
     private final Variable<Path> cwd;
-    private final JTextField cwdInput;
     private final JPanel jPanel;
 
     private volatile String lastText = "";
 
     private CWDPanel(final Context context) {
         cwd = context.cwd();
-        cwdInput = JTextFields.builder()
-                              .subscribe(Channel.FOCUS_GAINED, this::onInputFocusGained)
-                              .subscribe(Channel.FOCUS_LOST, this::onInputConfirmed)
-                              .subscribe(Channel.JTF_ACTION_PERFORMED, this::onInputConfirmed)
-                              .build();
         jPanel = JPanels.builder(new BorderLayout())
-                        .add(new WestPanel().ui(), BorderLayout.LINE_START)
-                        .add(cwdInput, BorderLayout.CENTER)
-                        .add(new EastPanel().ui(), BorderLayout.LINE_END)
+                        .add(new WestPanel().panel, BorderLayout.LINE_START)
+                        .add(new CenterPanel().panel, BorderLayout.CENTER)
+                        .add(new EastPanel().panel, BorderLayout.LINE_END)
                         .build();
-        cwd.subscribe(INIT, this::onSetCWD);
     }
 
     static CWDPanel by(final Context context) {
         return new CWDPanel(context);
-    }
-
-    private void onSetCWD(final Path path) {
-        cwdInput.setText(path.toString());
-    }
-
-    private void onInputFocusGained(final FocusEvent event) {
-        lastText = cwdInput.getText();
-    }
-
-    private void onInputConfirmed(final AWTEvent event) {
-        if (!Objects.equals(lastText, cwdInput.getText())) {
-            cwd.set(Path.of(cwdInput.getText()));
-            if (cwdInput.isFocusOwner()) {
-                cwdInput.transferFocus();
-                cwdInput.requestFocusInWindow();
-            }
-        }
     }
 
     final JPanel ui() {
@@ -86,17 +61,11 @@ final class CWDPanel {
             final JButton upButton = JButtons.builder()
                                              .setIcon(Ico.UPDIR)
                                              .build();
-            final JLabel symbol = new JLabel(Ico.OPNDIR);
             panel = JPanels.builder(new GridLayout(1, 4))
                            .add(bckButton)
                            .add(fwdButton)
                            .add(upButton)
-                           .add(symbol)
                            .build();
-        }
-
-        final JPanel ui() {
-            return panel;
         }
     }
 
@@ -113,9 +82,42 @@ final class CWDPanel {
                            .add(bckButton)
                            .build();
         }
+    }
 
-        final JPanel ui() {
-            return panel;
+    private final class CenterPanel {
+
+        private final JTextField textField;
+        private final JPanel panel;
+
+        private CenterPanel() {
+            textField = JTextFields.builder()
+                                   .subscribe(Channel.FOCUS_GAINED, this::onInputFocusGained)
+                                   .subscribe(Channel.FOCUS_LOST, this::onInputConfirmed)
+                                   .subscribe(Channel.JTF_ACTION_PERFORMED, this::onInputConfirmed)
+                                   .build();
+            panel = JPanels.builder(new BorderLayout())
+                           .add(new JLabel(Ico.OPNDIR), BorderLayout.LINE_START)
+                           .add(textField, BorderLayout.CENTER)
+                           .build();
+            cwd.subscribe(INIT, this::onSetCWD);
+        }
+
+        private void onSetCWD(final Path path) {
+            textField.setText(path.toString());
+        }
+
+        private void onInputFocusGained(final FocusEvent event) {
+            lastText = textField.getText();
+        }
+
+        private void onInputConfirmed(final AWTEvent event) {
+            if (!Objects.equals(lastText, textField.getText())) {
+                cwd.set(Path.of(textField.getText()));
+                if (textField.isFocusOwner()) {
+                    textField.transferFocus();
+                    textField.requestFocusInWindow();
+                }
+            }
         }
     }
 }
