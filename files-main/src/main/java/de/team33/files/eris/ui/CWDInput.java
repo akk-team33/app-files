b38e1518f90cfe283ab5a0e9ca5f3a791e4e81b6
Andi Kluge <akk-dev@team33.de>
2026-04-26 12:51:24 +0200

    Refined CWDInput

--- files-main/src/main/java/de/team33/files/eris/ui/CWDInput.java
+++ files-main/src/main/java/de/team33/files/eris/ui/CWDInput.java
@@ -2,11 +2,14 @@ package de.team33.files.eris.ui;
 
 import de.team33.patterns.serving.alpha.Variable;
 import de.team33.sphinx.luna.Channel;
+import de.team33.sphinx.metis.JButtons;
 import de.team33.sphinx.metis.JPanels;
 import de.team33.sphinx.metis.JTextFields;
+import net.team33.fscalc.ui.rsrc.Ico;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.nio.file.Path;
 
@@ -21,9 +24,12 @@ public final class CWDInput {
     private CWDInput(final Context context) {
         this.cwd = context.cwd();
         this.textField = JTextFields.builder()
+                                    .subscribe(Channel.JTF_ACTION_PERFORMED, this::onTextInput)
                                     .subscribe(Channel.FOCUS_LOST, this::onTextInput)
+                                    .subscribe(Channel.FOCUS_GAINED, this::onFocusGained)
                                     .build();
         this.panel = JPanels.builder(new BorderLayout())
+                            .add(newOptions(), BorderLayout.LINE_START)
                             .add(textField, BorderLayout.CENTER)
                             .build();
         cwd.subscribe(INIT, this::onSetCWD);
@@ -33,7 +39,26 @@ public final class CWDInput {
         return new CWDInput(context);
     }
 
-    private void onTextInput(final FocusEvent event) {
+    private Component newOptions() {
+        return JButtons.builder()
+                       .setIcon(Ico.UPDIR)
+                       .subscribe(Channel.ACTION_PERFORMED, this::onDirUp)
+                       .build();
+    }
+
+    private void onDirUp(final ActionEvent event) {
+        final Path parent = cwd.get().getParent();
+        if (null != parent) {
+            cwd.set(parent);
+        }
+    }
+
+    private void onFocusGained(final FocusEvent event) {
+        textField.setSelectionStart(0);
+        textField.setSelectionEnd(Integer.MAX_VALUE);
+    }
+
+    private void onTextInput(final Object event) {
         cwd.set(Path.of(textField.getText()));
     }
 
